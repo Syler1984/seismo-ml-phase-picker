cd1e4702ca3a5f5ca43c0f32b36e659b75b41dd6
Vladimir <syler1983.9@gmail.com>
2021-05-03 13:29:43 +1000

    Add write_batch function

--- picker.py
+++ picker.py
@@ -546,6 +546,34 @@ def slice_archives(archives, start, end):
     return traces
 
 
+def write_batch(path, dataset, batch, string = False):
+    """
+    Writes batch to h5 file
+    :param path: Path to h5 file
+    :param dataset: Name of the dataset
+    :param batch: Data
+    :param string: True - if data should be VL string
+    """
+    with h5.File(path, 'a') as file:
+
+        first = True
+        if dataset in file.keys():
+            first = False
+
+        if not first:
+            file[dataset].resize((file[dataset].shape[0] + batch.shape[0]), axis = 0)
+            file[dataset][-batch.shape[0]:] = batch
+        else:
+            if not string:
+                maxshape = list(batch.shape)
+                maxshape[0] = None
+                maxshape = tuple(maxshape)
+                file.create_dataset(dataset, data = batch, maxshape = maxshape, chunks = True)
+            else:
+                dt = h5.string_dtype(encoding='utf-8')
+                file.create_dataset(dataset, data = batch, maxshape = (None,), chunks = True, dtype = dt)
+
+
 if __name__ == '__main__':
 
     # Default params
